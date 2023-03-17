5b3f6ef37763b54045c3f7222957679f9bd4185e
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-17 14:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-  // Convert string to lowercase and remove non-alphanumeric characters
-  str = str.toLowerCase().replace(/[\W_]/g, '');
-  
-  // Check if string is equal to its reverse
-  return str === str.split('').reverse().join('');
-}
+ const string = str.toLowerCase();
+ const len = str.length;
 
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
 
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
